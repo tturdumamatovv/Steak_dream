3cc51eb9f1faa9749af587a2e0e8a02c20d40e51
ZTZ <tzamirov@gmail.com>
2024-10-18 11:11:45 +0600

    add translated fields to suppliers

--- apps/yaros_connector/models.py
+++ apps/yaros_connector/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Supplier(models.Model):
-    name = models.CharField(max_length=255)
-    url = models.CharField(max_length=255)
-    publication = models.CharField(max_length=255)
-    username = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    infosystem = models.CharField(max_length=255)
+    name = models.CharField(verbose_name=_('Название'), max_length=255)
+    url = models.CharField(verbose_name=_('Ссылка'), max_length=255)
+    publication = models.CharField(verbose_name=_('Публикация'), max_length=255)
+    username = models.CharField(verbose_name=_('Имя пользователя'), max_length=255)
+    password = models.CharField(verbose_name=_('Пароль'), max_length=255)
+    infosystem = models.CharField(verbose_name=_('Инфосистема'), max_length=255)
 
     def __str__(self):
         return self.name
