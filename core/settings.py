d29ea5eeb7a05f45c2f67fa45b831f4da23e5b99
ZTZ <tzamirov@gmail.com>
2024-10-19 13:26:34 +0600

    chatmessages migrations + add statisfiles dirs to settigns

--- core/settings.py
+++ core/settings.py
@@ -143,6 +143,9 @@ AUTH_USER_MODEL = "authentication.User"
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets'),
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
