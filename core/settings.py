885396460edf284c0a5ae4f620bdffec21265d6c
Nurmuhammed <muki2022@mail.ru>
2024-11-19 12:40:24 +0600

    changed access

--- core/settings.py
+++ core/settings.py
@@ -123,6 +123,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
